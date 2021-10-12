8c0a436723918fc0e246aea84666833663263e96
FranElfers <fransozzan@gmail.com>
2021-10-12 18:48:55 +0200

    ubicacion no interactiva en detalle

--- screens/FieldDetailScreen.js
+++ screens/FieldDetailScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Text, View, StyleSheet, ScrollView, Image } from 'react-native'
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 import db from '../database/firebase'
 
 const FieldDetailScreen = ({ route, navigation }) => {
@@ -17,18 +18,51 @@ const FieldDetailScreen = ({ route, navigation }) => {
 
 	React.useEffect(() => {
 		navigation.setOptions({ title: field.name })
+		console.log(field.location)
 	}, [field])
 	
 	return <ScrollView>
 		<View style={styles.cover}>
 			<Image style={styles.coverImage} source={{ uri:"https://i2.wp.com/hipertextual.com/wp-content/uploads/2014/03/windows_xp_bliss-wide.jpg?w=1560&ssl=1" }} />
-			<Text style={styles.coverTitle}>{field.name}</Text>
+			<Text style={styles.coverTitle}>{field.name} | ${field.price}</Text>
 		</View>
 		<View style={styles.container}>
-			<Text>{field.location}</Text>
-			<Text>{field.price}</Text>
+			<View style={styles.map}>
+				{field.price !== undefined && <MapView 
+					style={{ flex: 1 }}
+					provider={PROVIDER_GOOGLE}
+					showsUserLocation
+					scrollEnabled={false}
+					initialRegion={{
+						latitude: field.location[0],
+            longitude: field.location[1],
+            latitudeDelta: 0.00922,
+            longitudeDelta: 0.00421
+					}}
+				>
+					<Marker 
+						coordinate={{
+							latitude: field.location[0],
+							longitude: field.location[1]
+						}} 
+						image={require('../assets/mini_fulbo_marker.png')}
+					/>
+				</MapView>}
+			</View>
 		</View>
 	</ScrollView>
+}
+
+const boxShadow = {
+	shadowColor: "#000",
+	shadowOffset: {
+		width: 0,
+		height: 5,
+	},
+	shadowOpacity: 0.34,
+	shadowRadius: 6.27,
+
+	elevation: 10,
 }
 
 const styles = StyleSheet.create({
@@ -50,6 +84,13 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		padding: 15,
+	},
+	map: {
+		width: 200,
+		height: 200,
+		borderRadius: 15,
+		overflow: 'hidden',
+		...boxShadow
 	}
 })
 
